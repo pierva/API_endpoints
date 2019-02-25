76adbf37f95fadc05ab820e699bda73024a32967
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 21:23:10 -0500

    feat: Add new CRUD methods to the routes

--- endpoints.py
+++ endpoints.py
@@ -1,18 +1,43 @@
 from flask import Flask
 app = Flask(__name__)
-# Create the appropriate app.route functions. Test and see if they work
 
-#Make an app.route() decorator here for when the client sends the URI "/puppies"
 
-@app.route('/puppies')
+@app.route('/puppies', methods=['GET', 'POST'])
 def puppiesFunction():
-  return "Yes, puppies!"
+    if request.method == 'GET':
+        return getAllPuppies()
+    elif reques.method == 'POST':
+        return makeANewPuppy()
 
 
-#Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
-@app.route('/puppies/<int:id>')
+@app.route('/puppies/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    if request.method == 'GET':
+        return getPuppy(id)
+
+    if request.method == 'PUT':
+        return updatePuppy(id)
+
+    elif request.method == 'DELETE':
+        return deletePuppy(id)
+
+
 def puppiesFunctionId(id):
   return "This method will act on the puppy with id %s" % id
+
+def getAllPuppies():
+  return "Getting All the puppies!"
+
+def makeANewPuppy():
+  return "Creating A New Puppy!"
+
+def getPuppy(id):
+	return "Getting Puppy with id %s" % id
+
+def updatePuppy(id):
+  return "Updating a Puppy with id %s" % id
+
+def deletePuppy(id):
+  return "Removing Puppy with id %s" % id
 
 
 if __name__ == '__main__':
