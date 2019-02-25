9940a47fcf9cd04c1228d96c869e0f20f495d4dd
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 21:26:21 -0500

    fix: Import missing module, and fix typo

--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def puppiesFunction():
     if request.method == 'GET':
         return getAllPuppies()
-    elif reques.method == 'POST':
+    elif request.method == 'POST':
         return makeANewPuppy()
 
 
