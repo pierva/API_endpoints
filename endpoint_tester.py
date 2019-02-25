ec116c98a076421f27fe3d534308530ff0e481a9
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-24 21:24:42 -0500

    chore: Update tester file in order to test all the CRUD methods

--- endpoint_tester.py
+++ endpoint_tester.py
@@ -21,8 +21,27 @@ except Exception as err:
 else:
 	print "Test 1 PASS: Succesfully Made GET Request to /puppies"
 
+
+#Making a POST Request
+print "Making a POST request to /puppies..."
+try:
+	url = address + "/puppies"
+	h = httplib2.Http()
+	resp, result = h.request(url, 'POST')
+	if resp['status'] != '200':
+		raise Exception('Received an unsuccessful status code of %s' % resp['status'])
+
+except Exception as err:
+	print "Test 2 FAILED: Could not make POST Request to web server"
+	print err.args
+	sys.exit()
+else:
+	print "Test 2 PASS: Succesfully Made POST Request to /puppies"
+
+
 #Making GET Requests to /puppies/id
 print "Making GET requests to /puppies/id "
+
 try:
 	id = 1
 	while id <= 10:
@@ -34,9 +53,53 @@ try:
 		id = id + 1
 
 except Exception as err:
-	print "Test 2 FAILED: Could not make GET Request to /puppies/id"
+	print "Test 3 FAILED: Could not make GET Requests to web server"
 	print err.args
 	sys.exit()
 else:
-	print "Test 2 PASS: Succesfully Made GET Request to /puppies/id"
+	print "Test 3 PASS: Succesfully Made GET Request to /puppies/id"
+
+
+
+#Making a PUT Request
+print "Making PUT requests to /puppies/id "
+
+try:
+	id = 1
+	while id <= 10:
+		url = address + "/puppies/%s" % id
+		h = httplib2.Http()
+		resp, result = h.request(url, 'PUT')
+		if resp['status'] != '200':
+			raise Exception('Received an unsuccessful status code of %s' % resp['status'])
+		id = id + 1
+
+except Exception as err:
+	print "Test 4 FAILED: Could not make PUT Request to web server"
+	print err.args
+	sys.exit()
+else:
+	print "Test 4 PASS: Succesfully Made PUT Request to /puppies/id"
+
+
+#Making a DELETE Request
+
+print "Making DELETE requests to /puppies/id ... "
+
+try:
+	id = 1
+	while id <= 10:
+		url = address + "/puppies/%s" % id
+		h = httplib2.Http()
+		resp, result = h.request(url, 'DELETE')
+		if resp['status'] != '200':
+			raise Exception('Received an unsuccessful status code of %s' % resp['status'])
+		id = id + 1
+
+except Exception as err:
+	print "Test 5 FAILED: Could not make DELETE Requests to web server"
+	print err.args
+	sys.exit()
+else:
+	print "Test 5 PASS: Succesfully Made DELETE Request to /puppies/id"
 	print "ALL TESTS PASSED!!"
